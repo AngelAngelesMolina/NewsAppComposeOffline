7218d6daad886e60b63b1a51f13a6b5ec1043b6b
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2026-02-28 23:05:25 -0600

    CHG: App theme route

--- app/src/main/java/com/jaamcoding/newappcompose/core/presentation/MainActivity.kt
+++ app/src/main/java/com/jaamcoding/newappcompose/core/presentation/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.jaamcoding.newappcompose.ui.theme.NewAppComposeTheme
+import com.jaamcoding.newappcompose.core.presentation.ui.theme.NewAppComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            _root_ide_package_.com.jaamcoding.newappcompose.core.presentation.ui.theme.NewAppComposeTheme {
+            NewAppComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
