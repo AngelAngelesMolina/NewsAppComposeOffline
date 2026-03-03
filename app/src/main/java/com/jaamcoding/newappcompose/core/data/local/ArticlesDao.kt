23779a5ffb63ac1403c101e95f97bab95209c95e
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2026-03-02 18:09:43 -0600

    ADD: ArticleDao

---
+++ app/src/main/java/com/jaamcoding/newappcompose/core/data/local/ArticlesDao.kt
@@ -0,0 +1,22 @@
+package com.jaamcoding.newappcompose.core.data.local
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+
+@Dao
+interface ArticlesDao {
+
+    @Query("SELECT * FROM articleentity")
+    suspend fun getArticleList(): List<ArticleEntity>
+
+    @Upsert
+    suspend fun upsertArticleList(articleList: List<ArticleEntity>)
+
+    @Query("SELECT * FROM articleentity WHERE article_id = :articleId")
+    suspend fun getArticleById(articleId: String): ArticleEntity?
+
+    @Query("DELETE FROM articleentity")
+    suspend fun clearDatabase()
+
+}
