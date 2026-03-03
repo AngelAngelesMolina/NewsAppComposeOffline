c9f668040931b8f897bde6702ad8e49fd53e6b97
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2026-03-02 18:09:37 -0600

    ADD: ArticleEntry

---
+++ app/src/main/java/com/jaamcoding/newappcompose/core/data/local/ArticleEntity.kt
@@ -0,0 +1,17 @@
+package com.jaamcoding.newappcompose.core.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity
+data class ArticleEntity(
+    @PrimaryKey(autoGenerate = false)
+    val article_id: String,
+    val title: String,
+    val description: String,
+    val content: String,
+    val pubDate: String,
+    val source_name: String,
+    val image_url: String,
+)
