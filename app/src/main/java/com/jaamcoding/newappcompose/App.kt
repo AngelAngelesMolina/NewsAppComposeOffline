27758f1826fef1c7ddd026068e7d3857ff3bb7bd
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2026-03-02 18:20:24 -0600

    ADD: Dependency Injection configuration.

---
+++ app/src/main/java/com/jaamcoding/newappcompose/App.kt
@@ -0,0 +1,18 @@
+package com.jaamcoding.newappcompose
+
+import android.app.Application
+import com.jaamcoding.newappcompose.core.di.coreModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@App)
+            modules(coreModule)
+        }
+    }
+
+}
