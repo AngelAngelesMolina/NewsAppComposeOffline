0b4deec1f2c93467cd2041cfbcec06a06ac9176a
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2026-03-02 18:20:44 -0600

    ADD: ArticleDatabase.

---
+++ app/src/main/java/com/jaamcoding/newappcompose/core/data/local/ArticleDatabase.kt
@@ -0,0 +1,13 @@
+package com.jaamcoding.newappcompose.core.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+
+@Database(
+    entities = [ArticleEntity::class],
+    version = 1
+)
+abstract class ArticleDatabase : RoomDatabase() {
+    abstract val dao: ArticlesDao
+}
