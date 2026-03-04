2a14cfc3fd4cbe84c57686ca84bd75b69af5f1bd
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2026-03-04 17:49:29 -0600

    ADD: NewsViewModelImpl

--- app/src/main/java/com/jaamcoding/newappcompose/news/presentation/NewsViewModel.kt
+++ app/src/main/java/com/jaamcoding/newappcompose/news/presentation/NewsViewModel.kt
@@ -1,9 +1,90 @@
 package com.jaamcoding.newappcompose.news.presentation
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.jaamcoding.newappcompose.core.domain.NewsRepository
+import com.jaamcoding.newappcompose.core.domain.NewsResult
+import kotlinx.coroutines.launch
 
 class NewsViewModel(private val newsRepository: NewsRepository) : ViewModel() {
+
+    var state by mutableStateOf(NewsState())
+        private set
+
+    init {
+        loadNews()
+    }
+
+    fun onAction(action: NewsAction) {
+        when (action) {
+            is NewsAction.Paginate -> {
+                paginate()
+            }
+        }
+    }
+
+
+    private fun loadNews() {
+        viewModelScope.launch {
+            state = state.copy(
+                isLoading = true,
+            )
+            newsRepository.getNews().collect { newsResult ->
+                state = when (newsResult) {
+                    is NewsResult.Error -> {
+                        state.copy(
+                            isError = true,
+                        )
+                    }
+
+                    is NewsResult.Success -> {
+                        state.copy(
+                            isError = false,
+                            articleList = newsResult.data?.articles ?: emptyList(),
+                            nextPage = newsResult.data?.nextPage
+                        )
+                    }
+                }
+            }
+            state = state.copy(
+                isLoading = false,
+            )
+
+        }
+    }
+
+    private fun paginate() {
+        viewModelScope.launch {
+            state = state.copy(
+                isLoading = true,
+            )
+            newsRepository.paginate(state.nextPage).collect { newsResult ->
+                state = when (newsResult) {
+                    is NewsResult.Error -> {
+                        state.copy(
+                            isError = true,
+                        )
+                    }
+
+                    is NewsResult.Success -> {
+                        val articles = newsResult.data?.articles ?: emptyList()
+                        state.copy(
+                            isError = false,
+                            articleList = state.articleList + articles, //Adding to the list
+                            nextPage = newsResult.data?.nextPage
+                        )
+                    }
+                }
+            }
+            state = state.copy(
+                isLoading = false,
+            )
+
+        }
+    }
 
 
 }
