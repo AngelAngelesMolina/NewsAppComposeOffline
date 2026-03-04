368e6fd11d13afc54c2141c9d31e467b5aa6a58f
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2026-03-03 20:15:37 -0600

    ADD: NewsViewModel

---
+++ app/src/main/java/com/jaamcoding/newappcompose/news/presentation/NewsViewModel.kt
@@ -0,0 +1,9 @@
+package com.jaamcoding.newappcompose.news.presentation
+
+import androidx.lifecycle.ViewModel
+import com.jaamcoding.newappcompose.core.domain.NewsRepository
+
+class NewsViewModel(private val newsRepository: NewsRepository) : ViewModel() {
+
+
+}
